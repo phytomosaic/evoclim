3912899a8c178cc3074a6daf842bc6dfa8f526a7
phytomosaic <smithr2@oregonstate.edu>
2020-10-20 16:48:56 -0700

    Add pretty plotting for BAMM

--- R/03_bamm-outcomes.R
+++ R/03_bamm-outcomes.R
@@ -19,19 +19,24 @@ system( 'bamm --version' ) # BAMM 2.5.0 (2015-11-01)
 
 ### setup dirs and filenames
 setwd('/home/rob/prj/evoclim/bamm')   # change into working dir (R)
-fnm_phy     <- 'lichen_phy.tre'       # phylogeny
-fnm_pos_tra <- 'lichen_tra_pos.txt'   # traits (niche position)
-fnm_brd_tra <- 'lichen_tra_brd.txt'   # traits (niche breadth)
+fnm_phy       <- 'lichen_phy.tre'     # phylogeny
+fnm_pos_tra   <- 'lichen_tra_pos.txt' # traits (niche position)
+fnm_brd_tra   <- 'lichen_tra_brd.txt' # traits (niche breadth)
 fnm_pos_event <- 'pos_event_data.txt' # event data (niche position)
 fnm_brd_event <- 'brd_event_data.txt' # event data (niche breadth) 
 fnm_div_event <- 'div_event_data.txt' # event data (niche breadth) 
 fnm_pos_mcmc  <- 'pos_mcmc_out.txt'   # mcmc data (niche position)
 fnm_brd_mcmc  <- 'brd_mcmc_out.txt'   # mcmc data (niche breadth)
 fnm_div_mcmc  <- 'div_mcmc_out.txt'   # mcmc data (niche breadth)
-
+es            <- 34                   # expected number of shifts (set a priori)
 
 ### load tree and traits
-phy       <- read.tree(fnm_phy)      # load tree
+phy <- read.tree(fnm_phy)      # load tree
+tr  <- cbind(read.table(fnm_pos_tra,row.names=1),read.table(fnm_brd_tra)[,2])
+names(tr) <- c('pc_position','pc_breadth')
+head(tr)
+
+### PCA histograms
 png('../fig/fig_04_pca_histograms.png', wid=6.5, hei=3.25, units='in',
     bg='transparent', res=700)
 set_par_mercury(2)
@@ -41,27 +46,24 @@ hist(read.table(fnm_brd_tra)[,2], breaks=77, main='', xlab='PC1 niche breadths')
 text(c(-14, 0), c(150,150),c('Generalist','Specialist'), cex=0.7)
 dev.off()
 
-# ### do quick ancestral state reconstructions
-# lapply(1:NCOL(tr), function(j) { # ! ! ! TIMEWARN ! ! ! ~1-2 min per trait
-#   cat(paste0(j, ' of ', NCOL(tr), '... '))
-#   jnm <- j
-#   x   <- tr[,j]
-#   lim <- c(floor(min(x, na.rm=T)),ceiling(max(x, na.rm=T)))
-#   # continuous trait reconstruction across branches 
-#   trm <- phytools::contMap(p, x, lims=lim, plot=F)
-#   trm$cols[1:length(trm$cols)] <- viridis::inferno(length(trm$cols))
-#   # save output
-#   fnm <- paste0('./res/asr/asr_',sprintf("%02d", jnm),'__',
-#                 dimnames(tr)[[2]][jnm],'.rda')
-#   save(trm, file=fnm)
-#   # plot output (fan)
-#   fnm <- paste0('./fig/asr/fig_',sprintf("%02d", jnm),'_asr__',
-#                 dimnames(tr)[[2]][jnm],'.png')
-#   png(fnm, wid=8.5, hei=8.5, units='in', bg='transparent', res=500)
-#   plot(trm, lwd=1, outline=F, leg.txt=nm[jnm], type='fan', fsize=c(0.1,1))
-#   dev.off()
-# })
-
+### do quick ancestral state reconstructions for PCA scores
+trm <- lapply(1:NCOL(tr), function(j) { # ! ! ! TIMEWARN ! ! ! ~1-2 min per trait
+  cat(paste0(j, ' of ', NCOL(tr), '... '))
+  x   <- tr[,j]         # grab trait of interest
+  names(x) <- dimnames(tr)[[1]]  # assign species names
+  o <- phytools::contMap(phy, x, plot=F)  # continuous trait reconstruction
+  o$cols[1:length(o$cols)] <- viridis::inferno(length(o$cols))  # change colors
+  o
+})
+png('../fig/fig_05_anc_state_reconstrxn.png', wid=10.5, hei=5.25, units='in',
+    bg='transparent', res=700)
+set_par_mercury(2)
+plot(trm[[1]], lwd=0.5, outline=F, leg.txt='PC1 niche positions', 
+     type='fan', fsize=c(0.1,1))
+plot(trm[[2]], lwd=0.5, outline=F, leg.txt='PC1 niche breadths', 
+     type='fan', fsize=c(0.1,1))
+dev.off()
+rm(trm,tr)
 
 ### load BAMM event data
 ed_pos <- getEventData(phy = phy,
@@ -76,39 +78,49 @@ ed_brd <- getEventData(phy = phy,
                    nsamples  = 2000,  # 2000 per PNAS
                    verbose   = TRUE,
                    type      = 'trait')
-ed_div <- getEventData(phy = phy,
-                   eventdata = fnm_div_event,
-                   burnin    = 0.10,  # 10% per PNAS
-                   nsamples  = 2000,  # 2000 per PNAS
-                   verbose   = TRUE,
-                   type      = 'diversification')
+# ed_div <- getEventData(phy = phy,
+#                    eventdata = fnm_div_event,
+#                    burnin    = 0.10,  # 10% per PNAS
+#                    nsamples  = 2000,  # 2000 per PNAS
+#                    verbose   = TRUE,
+#                    type      = 'diversification')
 
 
 ### convergence diagnostics
 mc_pos <- read.csv(fnm_pos_mcmc, header=T)
-tail(mc_pos)
-burnstart <- floor(0.1 * nrow(mc_pos)) # discard first 10% of samples as burnin
-postburn  <- mc_pos[burnstart:nrow(mc_pos), ]
-coda::effectiveSize(postburn$N_shifts) # effective sample sizes of number of shift events in each sample
-coda::effectiveSize(postburn$logLik) # effective sample sizes of the log-likelihood 
-set_par_mercury(2)
-plot(mc_pos$logLik ~ mc_pos$generation) # sketch whether run converged 
-abline(v=mc_pos$generation[burnstart], col=2)
-plot(mc_pos$logPrior ~ mc_pos$generation)
-abline(v=mc_pos$generation[burnstart], col=2)
+mc_brd <- read.csv(fnm_brd_mcmc, header=T)
+# mc_div <- read.csv(fnm_div_mcmc, header=T)
+`convergence_diagnostics` <- function(mc_pos){
+  burnstart <- floor(0.1 * nrow(mc_pos)) # discard first 10% of samples as burnin
+  postburn  <- mc_pos[burnstart:nrow(mc_pos), ]
+  # effective sample sizes of number of shift events in each sample:
+  print(coda::effectiveSize(postburn$N_shifts))
+  # effective sample sizes of the log-likelihood:
+  print(coda::effectiveSize(postburn$logLik))
+  # sketch whether run converged 
+  set_par_mercury(2)
+  plot(mc_pos$logLik ~ mc_pos$generation) 
+  abline(v=mc_pos$generation[burnstart], col=2)
+  plot(mc_pos$logPrior ~ mc_pos$generation)
+  abline(v=mc_pos$generation[burnstart], col=2)
+}
+convergence_diagnostics(mc_pos)
+convergence_diagnostics(mc_brd)
+# convergence_diagnostics(mc_div)
+
 
 # compare prior and posterior
-set_par_mercury(1)
-plotPrior(mc_pos, expectedNumberOfShifts=34) 
-plotPrior(mc_pos, expectedNumberOfShifts=34) 
-plotPrior(mc_pos, expectedNumberOfShifts=34) 
+set_par_mercury(3)
+plotPrior(mc_pos, expectedNumberOfShifts=es) 
+plotPrior(mc_brd, expectedNumberOfShifts=es) 
+# plotPrior(mc_div, expectedNumberOfShifts=es) 
 
 
 ### identify 'best' configurations (3 alternatives):
 # ### alternative 1 --- maximize Bayes factors for all pairwise models
 # shift_probs <- summary(ed_pos)     # shift probabilities
 # pr_null     <- 1/NROW(mc_pos) # probability of null no-shift model
-# bfmat <- computeBayesFactors(mc_pos, expectedNumberOfShifts=34, burnin=0.1)
+# bfmat <- computeBayesFactors(mc_pos, expectedNumberOfShifts=es, burnin=0.1)
 # plot_heatmap(bfmat)
 # persp(bfmat, theta=135)
 # persp(bfmat[,1:11], theta=135)
@@ -130,72 +142,116 @@ plotPrior(mc_pos, expectedNumberOfShifts=34)
 #      method = 'polar',
 #      pal=viridis::inferno(99),
 #      logcolor=TRUE,
-#      # breaksmethod='quantile',  # breaksmethod='jenks'
 #      legend=T)
 # addBAMMshifts(msc_i, method = 'polar', cex=1, bg='gold')
 ### alternative 3 --- configuration that maximizes a posteriori probability
-best <- getBestShiftConfiguration(ed_pos, expectedNumberOfShifts=34)
-plot(best, 
+best_pos <- getBestShiftConfiguration(ed_pos, expectedNumberOfShifts=es)
+best_brd <- getBestShiftConfiguration(ed_brd, expectedNumberOfShifts=es)
+# best_div <- getBestShiftConfiguration(ed_div, expectedNumberOfShifts=es)
+NROW(best_pos$eventData[[1]]) # 31 distinct shifts in this configuration
+NROW(best_brd$eventData[[1]]) # 24 distinct shifts in this configuration
+# NROW(best_div$eventData[[1]]) # xxxxx distinct shifts in this configuration
+
+plot(best_pos, 
      method = 'polar',
      pal=viridis::inferno(99),
      logcolor=TRUE,
      # breaksmethod='quantile',  # breaksmethod='jenks'
      legend=T)
-addBAMMshifts(best, method = 'polar', cex=1, bg='gold')
-NROW(best$eventData[[1]]) # 31 distinct shifts in this configuration
-best$eventData # which nodes have distinct shifts
-getShiftNodesFromIndex(best)
-ape::axisPhylo()
 
+plot(ed_pos, 
+     method = 'polar', 
+     pal=viridis::inferno(99), 
+     breaksmethod = 'linear',
+     # logcolor=TRUE,
+     color.interval = c(0,3),
+     legend=F)
+addBAMMshifts(best_pos, method = 'polar', cex=1, bg='gold')
+
+best_pos$eventData # which nodes have distinct shifts
+sort(getShiftNodesFromIndex(best_pos,1)) # doesnt match line below
+best_pos$eventData[[1]]$node             # doesnt match line above...
+# ape::axisPhylo()
+# require(plotrix)
+# for(i in c(0,.4,.9, 1.1)) {
+#   plotrix::draw.circle(0,0,
+#                        radius=i,
+#                        col='#00000040',
+#                        border='transparent')
+# }
 
 
 ### mean, model-averaged rate of climatic trait evolution (for climate niche positions)
-png('../fig/fig_04_rate_niche_pos.png', wid=10.5, hei=10.0, units='in',
+png('../fig/fig_06_mean_rate_niche.png', wid=10.5, hei=5.25, units='in',
     bg='transparent', res=700)
-x <- plot(ed_pos, 
+set_par_mercury(2)
+plot(ed_pos, 
           method = 'polar', 
           pal=viridis::inferno(99), 
-          breaksmethod='quantile',  # breaksmethod='jenks'
-          legend=T) 
+          breaksmethod = 'linear',
+          # logcolor=TRUE,
+          color.interval = c(0,3),
+          legend=F)
+addBAMMshifts(best_pos, method = 'polar', cex=0.8, bg='gold', par.reset = F)
+title('Rates of evolution of\nclimate niche position', cex=0.7)
+plot(ed_brd, 
+          method = 'polar', 
+          pal=viridis::inferno(99), 
+          breaksmethod = 'linear',
+          # logcolor=TRUE,
+          color.interval = c(0,3),
+          legend=F)
+addBAMMshifts(best_brd, method = 'polar', cex=0.8, bg='gold', par.reset = F)
+title('Rates of evolution of\nclimate niche breadth', cex=0.7)
+# z <- plot(ed_div, 
+#           method = 'polar', 
+#           pal=viridis::inferno(99), 
+#           breaksmethod = 'linear',
+#           # logcolor=TRUE,
+#           color.interval = c(0,3),
+#           legend=F)
+# addBAMMshifts(best_div, method = 'polar', cex=1, bg='gold')
+# title('Rate of diversification')
 dev.off()
 # den <- x$colordens # rate densities
 
-
-# phylorate histogram 
-`rate_hist` <- function (phylorates, xlim=NULL, ...) {
-  j <-  c("coords", "colorbreaks","palette", "colordens")
-  stopifnot(identical(names(phylorates),j))
-  x <- phylorates$colordens[,1]
-  y <- phylorates$colordens[,2]
-  if(is.null(xlim)){
-    xlim <- c(min(x), max(x))
-  } 
-  plot(NA, xlim = xlim, ylim = c(0, max(y)), ylab='density', xlab='trait rates', ...)
-  segments(x, y, x, 0, lend = 2, col = phylorates$colordens[, 3], lwd = 3)
-  lines(x,y, col='grey')
-}
-set_par_mercury(1)
-rate_hist(x, xlim=c(0,2.2))
-
-
-### rate shift probabilities as branch lengths
-mst <- marginalShiftProbsTree(ed_pos)   # marginal shift probs tree
-cst <- cumulativeShiftProbsTree(ed_pos) # cumulative shift probs tree
-set_par_mercury(2)
-plot.phylo(mst, no.margin=TRUE, show.tip.label=FALSE, type='fan')
-plot.phylo(cst, no.margin=TRUE, show.tip.label=FALSE, type='fan')
-# ### plot subtrees while preserving the original rates (colorbreaks)
-# identify(phy) # locate node(s) graphically on tree...
-# i_node <- 4263
-# plot(subtreeBAMM(ed_pos, node = i_node), lwd = 1, colorbreaks = x$colorbreaks,
-#      pal=viridis::viridis(99))
+# ### phylorate histograms
+# `rate_hist` <- function (phylorates, xlim=NULL, ...) {
+#   j <-  c("coords", "colorbreaks","palette", "colordens")
+#   stopifnot(identical(names(phylorates),j))
+#   x <- phylorates$colordens[,1]
+#   y <- phylorates$colordens[,2]
+#   if(is.null(xlim)){
+#     xlim <- c(min(x), max(x))
+#   } 
+#   plot(NA, xlim = xlim, ylim = c(0, max(y)), ylab='density', xlab='trait rates', ...)
+#   segments(x, y, x, 0, lend = 2, col = phylorates$colordens[, 3], lwd = 3)
+#   lines(x,y, col='grey')
+# }
+# set_par_mercury(2)
+# rate_hist(x, xlim=c(0,3.2), add=T)
+# rate_hist(y, xlim=c(0,2.2), add=T)
+# rate_hist(z, xlim=c(0,2.2), add=T)
 
 
-### 95 % credible set of rate shift configurations sampled with BAMM 
-cset <- credibleShiftSet(ed_pos, expectedNumberOfShifts=34, threshold=3)
-summary(cset)
-cset$number.distinct
-plot.credibleshiftset(cset, method='polar')
+# ### rate shift probabilities as branch lengths
+# mst <- marginalShiftProbsTree(ed_pos)   # marginal shift probs tree
+# cst <- cumulativeShiftProbsTree(ed_pos) # cumulative shift probs tree
+# set_par_mercury(2)
+# plot.phylo(mst, no.margin=TRUE, show.tip.label=FALSE, type='fan')
+# plot.phylo(cst, no.margin=TRUE, show.tip.label=FALSE, type='fan')
+# # ### plot subtrees while preserving the original rates (colorbreaks)
+# # identify(phy) # locate node(s) graphically on tree...
+# # i_node <- 4263
+# # plot(subtreeBAMM(ed_pos, node = i_node), lwd = 1, colorbreaks = x$colorbreaks,
+# #      pal=viridis::viridis(99))
+
+
+# ### 95 % credible set of rate shift configurations sampled with BAMM 
+# cset <- credibleShiftSet(ed_pos, expectedNumberOfShifts=es, threshold=3)
+# summary(cset)
+# cset$number.distinct
+# plot.credibleshiftset(cset, method='polar')
 
 
 ### macroevolutionary cohort analysis:
